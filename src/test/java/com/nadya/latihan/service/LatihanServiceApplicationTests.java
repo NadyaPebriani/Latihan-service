dc154f257e4bb6a9931abe7fd419d87c562fc41d
Nadya Pebriani <113502682+NadyaPebriani@users.noreply.github.com>
2022-09-27 14:39:21 +0700

    Add files via upload

---
+++ src/test/java/com/nadya/latihan/service/LatihanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadya.latihan.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
